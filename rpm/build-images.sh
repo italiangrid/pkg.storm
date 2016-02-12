b53950e6784dcaec28d76d9f21851ad00235b475
enricovianello <maverick17586@gmail.com>
2016-02-12 12:27:36 +0100

    Fixed storm-grdiftp-dsi tag

--- rpm/build-images.sh
+++ rpm/build-images.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-components="storm-backend-server storm-webdav storm-client storm-native-libs"
+components="storm-backend-server \
+  storm-webdav \
+  storm-client \
+  storm-gridhttps-server \
+  yaim-storm \
+  storm-dynamic-info-provider \
+  storm-grdftp-dsi"
 
 for c in ${components}; do
   pushd $c
