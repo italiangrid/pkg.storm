2383ba1b94c686711312bbd313ae3df123b1b570
enricovianello <maverick17586@gmail.com>
2016-02-18 15:40:24 +0100

    Removed native libs gpfs repo download

--- rpm/storm-native-libs/pre-build.sh
+++ rpm/storm-native-libs/pre-build.sh
@@ -2,13 +2,9 @@
 set -ex
 
 # install addictional packages
-wget --no-check-certificate https://raw.githubusercontent.com/cnaf/ci-puppet-modules/master/modules/puppet-gpfs-repo/files/gpfs.repo -O /etc/yum.repos.d/gpfs.repo
 yum -y install pkgconfig \
   swig \
   libacl-devel \
   gpfs.base \
-  gpfs.docs \
-  gpfs.gpl \
-  gpfs.msg.en_US \
   lcmaps-without-gsi-devel \
   lcmaps-interface
