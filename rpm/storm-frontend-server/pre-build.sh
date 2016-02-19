becc3652f1997b89ee855fc48501094c56f52b98
enricovianello <maverick17586@gmail.com>
2016-02-19 15:00:16 +0100

    Fix frontend pre-build

--- rpm/storm-frontend-server/pre-build.sh
+++ rpm/storm-frontend-server/pre-build.sh
@@ -2,10 +2,25 @@
 set -ex
 
 # install addictional packages
-yum install -y gcc \
-  gcc-c++ \
-  make \
-  gsoap-devel \
+rpm --import http://emisoft.web.cern.ch/emisoft/dist/EMI/3/RPM-GPG-KEY-emi
+
+if [ $el_version == "5" ]; then
+
+  wget http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl5/x86_64/base/emi-release-3.0.0-2.el5.noarch.rpm
+  yum localinstall -y emi-release-3.0.0-2.el5.noarch.rpm
+
+  yum install -y boost141-devel
+
+else
+
+  wget --no-check-certificate http://emisoft.web.cern.ch/emisoft/dist/EMI/3/sl6/x86_64/base/emi-release-3.0.0-2.el6.noarch.rpm
+  yum localinstall -y emi-release-3.0.0-2.el6.noarch.rpm
+
+  yum install -y libuuid-devel boost-devel
+
+fi
+
+yum install -y pkgconfig \
   curl-devel \
   mysql-devel \
   globus-gssapi-gsi-devel \
@@ -13,7 +28,13 @@ yum install -y gcc \
   globus-common-devel \
   globus-gridmap-callout-error-devel \
   globus-gsi-credential-devel \
+  krb5-devel \
+  gsoap-devel \
   CGSI-gSOAP-devel \
+  libuuid-devel \
+  voms \
+  gcc \
+  gcc-c++ \
   storm-xmlrpc-c-devel \
   argus-pep-api-c \
   argus-pep-api-c-devel
@@ -21,13 +42,3 @@ yum install -y gcc \
 yum install -y redhat-lsb
 
 el_version=$(lsb_release -rs | cut -f1 -d.)
-
-if [ $el_version == "5" ]; then
-
-  yum install -y boost141-devel
-
-else
-
-  yum install -y libuuid-devel boost-devel
-
-fi
