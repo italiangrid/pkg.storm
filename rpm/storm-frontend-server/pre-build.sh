b28949d4a051aa6ba6361007294e651f3144b8f7
enricovianello <maverick17586@gmail.com>
2016-12-02 09:40:42 +0100

    Fixed EL6 and EL7 FE build issues after UDM repo migration

--- rpm/storm-frontend-server/pre-build.sh
+++ rpm/storm-frontend-server/pre-build.sh
@@ -10,18 +10,34 @@ if [ $el_version == "5" ]; then
 
   yum install --disableplugin=priorities -y boost141-devel
 
-  wget http://repository.egi.eu/sw/production/umd/3/sl5/x86_64/updates/umd-release-3.0.1-1.el5.noarch.rpm
-  yum localinstall --nogpgcheck -y umd-release-3.0.1-1.el5.noarch.rpm
+  rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
+  yum install -y http://repository.egi.eu/sw/production/umd/3/sl5/x86_64/updates/umd-release-3.0.1-1.el5.noarch.rpm
 
+  # storm-xmlrpc-c-devel is missing from UMD-3 repositories
   wget http://italiangrid.github.io/storm/repo/storm_sl5.repo -O /etc/yum.repos.d/storm_sl5.repo
 
-else
+fi
+
+if [ $el_version == "6" ]; then
 
   yum install -y libuuid-devel boost-devel
 
-  wget http://repository.egi.eu/sw/production/umd/3/sl6/x86_64/updates/umd-release-3.14.3-1.el6.noarch.rpm
-  yum localinstall --nogpgcheck -y umd-release-3.14.3-1.el6.noarch.rpm
+  rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
+  yum install -y http://repository.egi.eu/sw/production/umd/3/sl6/x86_64/updates/umd-release-3.14.3-1.el6.noarch.rpm
 
+  # storm-xmlrpc-c-c++ is broken into UMD-3 repositories
+  wget http://italiangrid.github.io/storm/repo/storm_sl6.repo -O /etc/yum.repos.d/storm_sl6.repo
+fi
+
+if [ $el_version == "7" ]; then
+
+  yum install -y libuuid-devel boost-devel
+
+  rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
+  yum install -y http://repository.egi.eu/sw/production/umd/4/centos7/x86_64/updates/umd-release-4.1.2-1.el7.centos.noarch.rpm
+
+  # storm is not currently included into UMD-4 repositories
+  wget http://italiangrid.github.io/storm/repo/storm_sl6.repo -O /etc/yum.repos.d/storm_sl6.repo
 fi
 
 yum install -y pkgconfig \
