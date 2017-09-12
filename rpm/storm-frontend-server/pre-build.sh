7179eed10dc3a86e59cd8ce9d981a5afc12276e2
enricovianello <maverick17586@gmail.com>
2017-09-12 16:35:39 +0200

    propagate UMD_REPO variable as env variable

--- rpm/storm-frontend-server/pre-build.sh
+++ rpm/storm-frontend-server/pre-build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -ex
 
-# We need UMD repos for Argus pep-c deps
-UMD_REPO_PACKAGE_EL6="http://repository.egi.eu/sw/production/umd/3/sl6/x86_64/updates/umd-release-3.14.3-1.el6.noarch.rpm"
-
 yum install -y libuuid-devel boost-devel
 
 rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
 yum install -y ${UMD_REPO_RPM}
 
 # We want to give more priority to the stage area repo than UMD
-sed -i "s/priority=1/priority=2/" /etc/yum.repos.d/UMD-3-base.repo /etc/yum.repos.d/UMD-3-updates.repo 
+sed -i "s/priority=1/priority=2/" /etc/yum.repos.d/UMD-*-base.repo /etc/yum.repos.d/UMD-*-updates.repo 
 
 if [ $el_version == "7" ]; then
 
