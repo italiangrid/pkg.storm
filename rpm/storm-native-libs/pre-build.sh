67247569415f5ea7e79a62f7b0259ce01c4e4ae1
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2018-05-15 18:13:59 +0200

    Packaging for storm-native-libs 1.0.5-2
    Setting ACLs in production on GPFS fails when native-libs are compiled
    agains GPFS 4.1.x. Reverting the build to GPFS 3.4.x seems to solve the
    issue.

--- rpm/storm-native-libs/pre-build.sh
+++ rpm/storm-native-libs/pre-build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -ex
 
-# install addictional packages
+# Override GPFS repo
+rm /etc/yum.repos.d/gpfs.repo
+wget -P /etc/yum.repos.d https://ci.cloud.cnaf.infn.it/job/repo_gpfs/lastSuccessfulBuild/artifact/gpfs.repo
+
 yum -y install pkgconfig \
   swig \
   libacl-devel \
-  gpfs.base \
+  gpfs.base-3.4.0 \ # Force use of 3.4.0
   lcmaps-without-gsi-devel \
   lcmaps-interface
