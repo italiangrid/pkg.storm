4eb9c99404e66b8ef2c57718609e88559b835089
enricovianello <maverick17586@gmail.com>
2018-05-22 10:17:50 +0200

    Removed the override of gpfs repo

--- rpm/storm-native-libs/pre-build.sh
+++ rpm/storm-native-libs/pre-build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -ex
 
-# Override GPFS repo
-rm /etc/yum.repos.d/gpfs.repo
-wget -P /etc/yum.repos.d https://ci.cloud.cnaf.infn.it/job/repo_gpfs/lastSuccessfulBuild/artifact/gpfs.repo
-
 yum -y install pkgconfig \
   swig \
   libacl-devel \
