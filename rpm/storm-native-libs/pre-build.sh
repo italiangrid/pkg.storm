e6cd889a0052a4ed670423d1256cc2e63768c514
enricovianello <maverick17586@gmail.com>
2019-02-27 13:45:17 +0100

    Move dkms install before installing gpfs-base

--- rpm/storm-native-libs/pre-build.sh
+++ rpm/storm-native-libs/pre-build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ex
 
+yum -y install dkms
+
 yum -y install pkgconfig \
   swig \
   libacl-devel \
   gpfs.base-3.4.0 \
   lcmaps-without-gsi-devel \
-  lcmaps-interface \
-  dkms
+  lcmaps-interface
