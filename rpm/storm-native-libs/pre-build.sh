5a8697711c1281264c79710254687ece2ecc55bd
enricovianello <maverick17586@gmail.com>
2019-02-27 14:33:23 +0100

    Tentative fix for modutils requirement

--- rpm/storm-native-libs/pre-build.sh
+++ rpm/storm-native-libs/pre-build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -ex
 
-yum -y install dkms
+yum localinstall -y http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
+yum install dkms -y
 
 yum -y install pkgconfig \
   swig \
