8f80e8460e6b93fdd95cbbb4f53fb50bb3b0b597
enricovianello <maverick17586@gmail.com>
2019-11-14 15:45:16 +0100

    fixed name

--- setup-volumes.sh
+++ setup-volumes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-PACKAGES_VOLUME=${PACKAGES_VOLUME:-packages-volume-pkg.example}
-STAGE_AREA_VOLUME=${STAGE_AREA_VOLUME:-stage-area-volume-pkg.example}
-MVN_REPO_VOLUME=${MVN_REPO_VOLUME:-mvn-repo-volume-pkg.example}
+PACKAGES_VOLUME=${PACKAGES_VOLUME:-packages-volume-pkg.storm}
+STAGE_AREA_VOLUME=${STAGE_AREA_VOLUME:-stage-area-volume-pkg.storm}
+MVN_REPO_VOLUME=${MVN_REPO_VOLUME:-mvn-repo-volume-pkg.storm}
 
 export PACKAGES_VOLUME=$(docker volume create ${PACKAGES_VOLUME})
 export STAGE_AREA_VOLUME=$(docker volume create ${STAGE_AREA_VOLUME})
