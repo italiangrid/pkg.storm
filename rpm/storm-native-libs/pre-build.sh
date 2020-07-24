b4e15939a6a16cdacb22a308f47f1aa843277fe7
enricovianello <maverick17586@gmail.com>
2020-07-24 19:06:44 +0200

    Fixed native libs build for centos7

--- rpm/storm-native-libs/pre-build.sh
+++ rpm/storm-native-libs/pre-build.sh
@@ -2,11 +2,26 @@
 set -ex
 
 yum localinstall -y http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-yum install dkms -y
+yum install redhat-lsb-core -y
 
-yum -y install pkgconfig \
-  swig \
-  libacl-devel \
-  gpfs.base-3.4.0 \
-  lcmaps-without-gsi-devel \
-  lcmaps-interface
+majversion=$(lsb_release -rs | cut -f1 -d.)
+
+if $majversion=='6'
+then
+  yum -y install pkgconfig \
+    swig \
+    libacl-devel \
+    gpfs.base-3.4.0 \
+    lcmaps-without-gsi-devel \
+    lcmaps-interface
+else
+  # missing modutils for centos7
+  yum install module-init-tools -y
+  rpm -Uvh --nodeps $(repoquery --location gpfs.base-3.4.0)
+
+  yum -y install pkgconfig \
+    swig \
+    libacl-devel \
+    lcmaps-without-gsi-devel \
+    lcmaps-interface
+fi
