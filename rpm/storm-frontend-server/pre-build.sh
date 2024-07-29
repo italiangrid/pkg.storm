2c044a7d36be9edb1e93388d8debd661e9f0a947
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2024-07-29 10:11:51 +0200

    Fix pre-build

--- rpm/storm-frontend-server/pre-build.sh
+++ rpm/storm-frontend-server/pre-build.sh
@@ -2,22 +2,10 @@
 set -ex
 
 #rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
-yum install -y yum-utils yum-priorities redhat-lsb
-
-#majversion=$(lsb_release -rs | cut -f1 -d.)
-
-# Install UMD repositories
-#if [ $majversion = "7" ]; then
-#  yum localinstall -y http://repository.egi.eu/sw/production/umd/4/centos7/x86_64/updates/umd-release-4.1.3-1.el7.centos.noarch.rpm
-#else
-#  yum localinstall -y http://repository.egi.eu/sw/production/umd/4/sl6/x86_64/updates/umd-release-4.1.3-1.el6.noarch.rpm
-#fi
+yum install -y yum-utils
 
 # Install StoRM stable repository
 yum-config-manager --add-repo https://repo.cloud.cnaf.infn.it/repository/storm/storm-stable-redhat9.repo
-
-# We want to give more priority to the stage area repo than UMD
-#sed -i "s/priority=1/priority=2/" /etc/yum.repos.d/UMD-*-base.repo /etc/yum.repos.d/UMD-*-updates.repo 
 
 yum install -y pkgconfig \
   curl-devel \
