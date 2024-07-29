9b8d226381eda0fbd79a3df663d1df4bbe37de7b
Enrico Vianello <enrico.vianello@cnaf.infn.it>
2024-07-29 10:06:20 +0200

    RedHat9 release candidate

--- rpm/storm-frontend-server/pre-build.sh
+++ rpm/storm-frontend-server/pre-build.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 set -ex
 
-rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
+#rpm --import http://repository.egi.eu/sw/production/umd/UMD-RPM-PGP-KEY
 yum install -y yum-utils yum-priorities redhat-lsb
 
-majversion=$(lsb_release -rs | cut -f1 -d.)
+#majversion=$(lsb_release -rs | cut -f1 -d.)
 
 # Install UMD repositories
-if [ $majversion = "7" ]; then
-  yum localinstall -y http://repository.egi.eu/sw/production/umd/4/centos7/x86_64/updates/umd-release-4.1.3-1.el7.centos.noarch.rpm
-else 
-  yum localinstall -y http://repository.egi.eu/sw/production/umd/4/sl6/x86_64/updates/umd-release-4.1.3-1.el6.noarch.rpm
-fi
+#if [ $majversion = "7" ]; then
+#  yum localinstall -y http://repository.egi.eu/sw/production/umd/4/centos7/x86_64/updates/umd-release-4.1.3-1.el7.centos.noarch.rpm
+#else
+#  yum localinstall -y http://repository.egi.eu/sw/production/umd/4/sl6/x86_64/updates/umd-release-4.1.3-1.el6.noarch.rpm
+#fi
 
 # Install StoRM stable repository
-if [ $majversion = "7" ]; then
-  yum-config-manager --add-repo https://repo.cloud.cnaf.infn.it/repository/storm/storm-stable-centos7.repo
-else
-  yum-config-manager --add-repo https://repo.cloud.cnaf.infn.it/repository/storm/storm-stable-centos6.repo
-fi
+yum-config-manager --add-repo https://repo.cloud.cnaf.infn.it/repository/storm/storm-stable-redhat9.repo
 
 # We want to give more priority to the stage area repo than UMD
-sed -i "s/priority=1/priority=2/" /etc/yum.repos.d/UMD-*-base.repo /etc/yum.repos.d/UMD-*-updates.repo 
+#sed -i "s/priority=1/priority=2/" /etc/yum.repos.d/UMD-*-base.repo /etc/yum.repos.d/UMD-*-updates.repo 
 
 yum install -y pkgconfig \
   curl-devel \
@@ -39,6 +35,4 @@ yum install -y pkgconfig \
   voms \
   gcc \
   gcc-c++ \
-  storm-xmlrpc-c-devel \
-  argus-pep-api-c \
-  argus-pep-api-c-devel
+  storm-xmlrpc-c-devel
